f6cb57d8cfba9231dd1b1359c22bcc728e9d1b35
Aydar <contact@fetchdev.host>
2020-10-11 16:51:46 +0300

    Changed script a bit

--- Misc/postsjsonparse.sh
+++ Misc/postsjsonparse.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
+#postsjsonparse.sh
+#COPYRIGHT LESTERRRY, 2020
+#Script parses postsfromgroup.sh output to single piece of text, 
+# which ill teach my neural network on.
+#Make sure you have 'jq' dependency installed.
+
 out="/OUT.txt"
 in="/bin/!out.json"
 text=$(cat $in | jq '.data[].response.items[].text')
-#text=$(cat $out)
-echo 1
+echo "1/4"
 text=$(sed 's/;//g' <<<"$text")
+echo "2/4"
 text=$(sed 's/\\n//g' <<<"$text")
 text=$(sed 's/\\//g' <<<"$text")
+echo "3/4"
 text=$(sed 's/#ask//g' <<<"$text")
 text=$(sed 's/#лс//g' <<<"$text")
-echo 2
-#text=$(sed 's/\" \"/J/g' <<<"$text")
-echo 3
+echo "4/4"
 text=$(sed 's/"//g' <<<"$text")
-echo 4
 echo $text > $out
