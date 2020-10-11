77c919c6f7b5a22b1d0c757a66ab076b2987e825
Aydar <contact@fetchdev.host>
2020-10-11 20:21:43 +0300

    Made things easier

--- Misc/postsjsonparse.sh
+++ Misc/postsjsonparse.sh
@@ -8,14 +8,6 @@
 out="/OUT.txt"
 in="/bin/!out.json"
 text=$(cat $in | jq '.data[].response.items[].text')
-echo "1/4"
-text=$(sed 's/;//g' <<<"$text")
-echo "2/4"
-text=$(sed 's/\\n//g' <<<"$text")
-text=$(sed 's/\\//g' <<<"$text")
-echo "3/4"
-text=$(sed 's/#ask//g' <<<"$text")
-text=$(sed 's/#лс//g' <<<"$text")
-echo "4/4"
-text=$(sed 's/"//g' <<<"$text")
+text=$(sed 's/[^[:alnum:][:space:].,!?:-]//g' <<<"$text")
+text=$(sed 's/[a-zA-Z]//g' <<<"$text")
 echo $text > $out
